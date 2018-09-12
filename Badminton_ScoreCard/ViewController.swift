896a7f7462bed5b463529ef84b6b88f9a2c8ff15
Mohankumar <nmohankumar.cse@gmail.com>
2018-09-12 12:15:26 +0530

    Initial Commit

---
+++ Badminton_ScoreCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Badminton_ScoreCard
+//
+//  Created by Mohankumar on 12/09/18.
+//  Copyright © 2018 Mohankumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
