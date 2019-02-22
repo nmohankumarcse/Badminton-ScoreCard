432cdbacc9c001803b86a216796acfd597033dba
Mohankumar N <nmohankumar.cse@hotmail.com>
2019-02-22 17:32:48 +0530

    Final commit

--- Badminton_ScoreCard/ViewController.swift
+++ Badminton_ScoreCard/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redGame: UILabel!
+    @IBOutlet weak var blueGame: UILabel!
+    
+    var blue : Int = 0
+    var red : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,37 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func outerRedTouched(_ sender: Any) {
+        incrementRed()
+    }
+    
+    @IBAction func innerRedTouched(_ sender: Any) {
+        incrementBlue()
+    }
+    
+    @IBAction func outerBlueTouched(_ sender: Any) {
+        incrementBlue()
+    }
+    
+    @IBAction func innerBlueTouched(_ sender: Any) {
+        incrementRed()
+    }
+    @IBAction func netRedtouched(_ sender: Any) {
+        incrementBlue()
+    }
+    @IBAction func netBlueTouched(_ sender: Any) {
+        incrementRed()
+    }
+    
+    func incrementBlue(){
+        blue = blue + 1
+        blueGame.text = "\(blue)"
+    }
+    
+    func incrementRed(){
+        red = red + 1
+        redGame.text = "\(red)"
+    }
 }
 
